80a7f76849ca7b1afcc6a83dd038052f50a7b394
knigawkl <lukasz.knigawka@outlook.com>
2019-03-12 14:40:56 +0100

    Dodaje dzialajacego czebyszka

--- interpolacja/lagrange_czeb.m
+++ interpolacja/lagrange_czeb.m
@@ -1,25 +1,26 @@
-% Lagrange's interpolation
+%%
 hold on;
-title('Interpolacja wielomianowa Lagrange''a z zastosowaniem wêz³ów równoodleg³ych dla funkcji zadanej w zadaniu');
+title('Interpolacja wielomianowa Lagrange''a z zastosowaniem wêz³ów Czebyszewa dla funkcji zadanej w zadaniu');
 xlabel('x');
 ylabel('f(x)');
 grid on;
 grid minor;
 samples=linspace(-1,1,1111);
 
-points_x=splitInterval(5);
+points_x=chebyshev(5);
+points_x=sort(points_x);
 points_y=getValues(points_x);
 plot(samples, lagrange(samples, points_x, points_y), 'b');
 
-points_x=splitInterval(9);
+points_x=chebyshev(9);
 points_y=getValues(points_x);
 plot(samples, lagrange(samples, points_x, points_y), 'k');
 
-points_x=splitInterval(11);
+points_x=chebyshev(11);
 points_y=getValues(points_x);
 plot(samples, lagrange(samples, points_x, points_y), 'r');
 
-points_x=splitInterval(15);
+points_x=chebyshev(15);
 points_y=getValues(points_x);
 plot(samples, lagrange(samples, points_x, points_y), 'c');
 
@@ -27,18 +28,6 @@ legend('5 wêz³ów', '9 wêz³ów', '11 wêz³ów', '15 wêz³ów');
 
 hold off;
 
-%%
-function x = splitInterval(node_count)
-    x=linspace(-1,1,node_count);
-end
-%%
-function y = getValues(x)
-    len=size(x,2);
-    y=zeros(len,1);
-    for i=1:size(x,1)
-        y(i)=2/(2+x(i)^9)+1;
-    end
-end
 %%
 function y = lagrange(x, points_x, points_y)
     n=size(points_x, 2);
@@ -53,5 +42,21 @@ function y = lagrange(x, points_x, points_y)
     y=0;
     for i=1:n
         y=y+points_y(i)*L(i,:);
+    end
+end
+%%
+function y = getValues(x)
+    len=size(x,2);
+    y=zeros(len,1);
+    for i=1:size(x,2)
+        y(i)=2/(2+x(i)^9)+1;
+    end
+    disp(size(x,2))
+end
+%%
+function x = chebyshev(len)
+    x=linspace(0,1,len);
+    for i=1:len
+        x(i) = cos(((2*i-1)*pi)/(2*len));
     end
 end
