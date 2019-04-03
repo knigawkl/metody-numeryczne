daa4597c78be59d26a2e23d603ae034e7a43e367
knigawkl <lukasz.knigawka@outlook.com>
2019-04-03 20:53:29 +0200

    Adding gaussian ellimination

--- uklady-rownan/gauss_pp.m
+++ uklady-rownan/gauss_pp.m
@@ -1,3 +1,29 @@
-% rozwiazuje dowolny uklad rownan liniowych metoda eliminacji gaussa
-% zaimplementowano algorytm wyboru elementu glownego
-
+%% wprowadzenie danych
+A=[2 1 5 -1.5;5 3 2 4;-2 3 2 -4;4.5 3 5 2];
+b=[1.7;47.3;-46.4;34.95];
+if size(A,1)~=size(A,2)
+    disp('Macierz nie jest kwadratowa!')
+    A=[];
+    return
+end
+check=A\b;
+%% eliminacja gaussa
+n=length(A);
+for i=1:n
+   for j=i+1:n       
+       k=A(j,i)/A(i,i);
+       A(j,:)=A(j,:)-A(i,:)*k;
+       b(j)=b(j)-b(i)*k;
+   end
+end
+%% podstawianie wsteczne
+x=zeros(n,1);
+for i=n:-1:1
+    sum=0.0;
+    for j=i:n
+        sum=sum+A(i,j)*x(j);
+    end
+    x(i)=(b(i)-sum)/A(i,i);
+end
+niepewnosc=1e-10;
+sprawdzenie=all(abs(check(:)-x(:)) <= niepewnosc);
