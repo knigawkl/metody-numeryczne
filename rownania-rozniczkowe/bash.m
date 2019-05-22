5514d09c43151baf87b722de56d0580af60c9f6c
smolinm1 <matsmolinski@gmail.com>
2019-05-22 21:09:28 +0200

    fafnasty

--- rownania-rozniczkowe/bash.m
+++ rownania-rozniczkowe/bash.m
@@ -8,17 +8,17 @@ v0=1;
 t=0:h:10;
 point_count=length(t);
 
-er=euler(h,t0,x0,v0,point_count);
-hr=heun(h,t0,x0,v0,point_count);
+b2=bash(h,t0,x0,v0,point_count, 2);
+b3=bash(h,t0,x0,v0,point_count, 3);
 
 f=@(x)(sin(x));
 y=arrayfun(f, t);
 
-plot(er(1, :), er(2, :), 'o', hr(1, :), hr(2, :), 'x', t, y);
+plot(b2(1, :), b2(2, :), 'o', b3(1, :), b3(2, :), 'x', t, y);
 grid on;
 grid minor;
 title('Porównanie rozwi¹zañ uk³adu równañ ró¿niczkowych');
-legend('Wynik metody Eulera','Wynik metody Heuena','Wynik analityczny','location','best');
+legend('Wynik Adamsa-Bashfortha 2 rzêdu','Wynik Adamsa-Bashfortha 3 rzêdu','Wynik analityczny','location','best');
 xlabel('x');
 ylabel('f(x)');
 ax=gca;
@@ -27,9 +27,9 @@ ax.YAxisLocation="origin";
 hold on;
 
 %% errors
-er_err=abs(y-er(2, :));
+er_err=abs(y-b2(2, :));
 max_er_err=max(er_err);
-hr_err=abs(y-hr(2,:));
+hr_err=abs(y-b3(2,:));
 max_hr_err= max(hr_err);
 
 %% euler's method
@@ -48,7 +48,7 @@ function euler_results = euler(h,t0,x0,v0,point_count)
 end
 
 %% adams-bash
-function bash_results = bash(h,t0,x0,v0,point_count,step)
+function bash_results = bash(h,t0,x0,v0,point_count,step) %specjalna dedykacja dla ATO
     bash_results=zeros(3,point_count);
     bash_results(:,1)=[t0;x0;v0];
     for i=2:step
@@ -60,26 +60,20 @@ function bash_results = bash(h,t0,x0,v0,point_count,step)
         v=v_prev+h*(-x_prev);
         bash_results(:,i)=[t,x,v];
     end
-    if step == 2
-        for i=step+1:point_count 
-            t_prev=bash_results(1,i-1);
-            x_prev=bash_results(2,i-1);
-            v_prev=bash_results(3,i-1);
-            t=t_prev+h;
+    
+    for i=step+1:point_count 
+        t_prev=bash_results(1,i-1);
+        x_prev=bash_results(2,i-1);
+        v_prev=bash_results(3,i-1);
+        t=t_prev+h;
+        if step == 2
             x=x_prev+h*(3*bash_results(2,i-1)+(-1)*bash_results(2,i-2));
             v=v_prev+h*(3*bash_results(3,i-1)+(-1)*bash_results(2,i-2));
-            bash_results(:,i)=[t,x,v];
         end
-    end  
-    if step == 3
-        for i=step+1:point_count 
-            t_prev=bash_results(1,i-1);
-            x_prev=bash_results(2,i-1);
-            v_prev=bash_results(3,i-1);
-            t=t_prev+h;
+        if step == 3
             x=x_prev+h*(23*bash_results(2,i-1)+(-16)*bash_results(2,i-2)+5*bash_results(2,i-3));
             v=v_prev+h*(23*bash_results(3,i-1)+(-16)*bash_results(3,i-2)+5*bash_results(3,i-3));
-            bash_results(:,i)=[t,x,v];
         end
-    end 
+        bash_results(:,i)=[t,x,v];    
+    end                   
 end
